3469a755ef2eef391b24863742de290a8141cd69
Ozdy <ozdy00@gmail.com>
2023-09-21 19:42:14 -0600

    Se ha solucionado la sintaxis de variables, se agregaron tokens y otra fila a la tabla

--- lexico.cpp
+++ lexico.cpp
@@ -6,13 +6,47 @@
 using namespace std;
 
 bool esPalabraClave(const char* str) {
-    const char* palabrasClave[] = { "if", "else", "while", "return", "int", "void", "float", "cout", "endl" };
+    const char* palabrasClave[] = { "if", "else", "while", "return", "cout", "endl" };
     for (const char* palabra : palabrasClave) {
         if (strcmp(str, palabra) == 0) {
             return true;
         }
     }
     return false;
+}
+
+bool esTipo(const char* str) {
+    const char* tipo[] = { "int", "float", "void" };
+    for (const char* palabra : tipo) {
+        if (strcmp(str, palabra) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool contieneLetras(const char* texto) {
+    int longitud = strlen(texto);
+    for (int i = 0; i < longitud; i++) {
+        if (isalpha(texto[i])) {
+            return true; // El texto contiene al menos una letra
+        }
+    }
+    return false; // El texto no contiene letras
+}
+
+bool contienePuntos(const char* texto) {
+    int longitud = strlen(texto);
+    int puntos = 0;
+    for (int i = 0; i < longitud; i++) {
+        if (texto[i] == '.') {
+            puntos++;
+            if (puntos > 1) {
+                return true; // El texto contiene al menos un punto
+            }
+        }
+    }
+    return false; // El texto no contiene puntos
 }
 
 bool esNumero(const char* str) {
@@ -46,11 +80,12 @@ bool esCadenaTexto(const char* str) {
     return len >= 2 && str[0] == '"' && str[len - 1] == '"';
 }
 
-void imprimirTabla(const char* elemento, const char* tipo) {
+void imprimirTabla(const char* elemento, const char* tipo, const char* numTipo) {
     const int anchoElemento = 25;
     const int anchoTipo = 20;
+    const int anchoNumTipo = 25;
 
-    cout << left << setw(anchoElemento) << elemento << " => " << setw(anchoTipo) << tipo << endl;
+    cout << left << setw(anchoElemento) << elemento << " => " << setw(anchoTipo) << tipo << setw(anchoNumTipo) << numTipo << endl;
 }
 
 void analizar(const char* str) {
@@ -74,68 +109,68 @@ void analizar(const char* str) {
                 strncpy(cadenaTexto, &str[i], j - i + 1);
                 cadenaTexto[j - i + 1] = '\0';
                 if (esCadenaTexto(cadenaTexto)) {
-                    imprimirTabla(cadenaTexto, "Cadena de Texto");
+                    imprimirTabla(cadenaTexto, "Cadena de Texto", "3");
                 } else {
-                    imprimirTabla(cadenaTexto, "Error: Cadena no válida");
+                    imprimirTabla(cadenaTexto, "Error: Cadena no válida", "ERROR");
                 }
                 i = j + 1;
             } else {
-                imprimirTabla("\"", "Error: Cadena no cerrada");
+                imprimirTabla("\"", "Error: Cadena no cerrada", "ERROR");
                 i++;
             }
         }
-        
+
         // Verificar si es un operador
         else if (str[i] == '+') {
-            imprimirTabla("+", "Operador de Adición");
+            imprimirTabla("+", "Operador de Adición", "5");
             i++;
         } else if (str[i] == '-') {
-            imprimirTabla("-", "Operador de Sustracción");
+            imprimirTabla("-", "Operador de Sustracción", "5");
             i++;
         } else if (str[i] == '*') {
-            imprimirTabla("*", "Operador de Multiplicación");
+            imprimirTabla("*", "Operador de Multiplicación", "6");
             i++;
         } else if (str[i] == '/') {
-            imprimirTabla("/", "Operador de División");
+            imprimirTabla("/", "Operador de División", "6");
             i++;
         } else if (str[i] == '=') {
             if (i + 1 < len && str[i + 1] == '=') {
-                imprimirTabla("==", "Operador Relacional");
+                imprimirTabla("==", "Operador Relacional", "11");
                 i += 2;
             } else {
-                imprimirTabla("=", "Operador de Asignación");
+                imprimirTabla("=", "Operador de Asignación", "18");
                 i++;
             }
         } else if (str[i] == '<' && i + 1 < len && str[i + 1] == '<') {
-            imprimirTabla("<<", "Operador de Inserción");
+            imprimirTabla("<<", "Operador de Inserción", "26");
             i += 2;
         } else if (str[i] == '>' && i + 1 < len && str[i + 1] == '>') { //> int main <
-            imprimirTabla(">>", "Operador de Extracción");
+            imprimirTabla(">>", "Operador de Extracción", "27");
             i += 2;
         } else if (str[i] == '<' || str[i] == '>' || (i + 1 < len && (str[i] == '=' || (str[i] == '&' && str[i + 1] == '&') || (str[i] == '|' && str[i + 1] == '|') || str[i] == '!'))) {
             if (str[i] == '<') {
-                imprimirTabla("<", "Operador Relacional");
+                imprimirTabla("<", "Operador Relacional", "7");
                 i++;
             } else if (str[i] == '>') {
-                imprimirTabla(">", "Operador Relacional"); 
+                imprimirTabla(">", "Operador Relacional", "7");
                 i++;
             } else if (str[i] == '<') {
-                imprimirTabla("<<", "Operador Relacional");
+                imprimirTabla("<<", "Operador Relacional", "26");
                 i += 2;
             } else if (str[i] == '>') {
-                imprimirTabla(">>", "Operador Relacional");
+                imprimirTabla(">>", "Operador Relacional", "27");
                 i += 2;
             } else if (str[i] == '=') {
-                imprimirTabla("==", "Operador Relacional");
+                imprimirTabla("==", "Operador Relacional", "11");
                 i += 2;
             } else if (str[i] == '&' && str[i + 1] == '&') {
-                imprimirTabla("&&", "Operador Lógico AND");
+                imprimirTabla("&&", "Operador Lógico AND", "9");
                 i += 2;
             } else if (str[i] == '|' && str[i + 1] == '|') {
-                imprimirTabla("||", "Operador Lógico OR");
+                imprimirTabla("||", "Operador Lógico OR", "8");
                 i += 2;
             } else if (str[i] == '!') {
-                imprimirTabla("!", "Operador Lógico NOT");
+                imprimirTabla("!", "Operador Lógico NOT", "10");
                 i++;
             }
         }
@@ -143,60 +178,68 @@ void analizar(const char* str) {
 
         // Verificar si es un símbolo
         else if (str[i] == '(') {
-            imprimirTabla("(", "Símbolo (Paréntesis)");
+            imprimirTabla("(", "Símbolo (Paréntesis)", "14");
             i++;
         } else if (str[i] == ')') {
-            imprimirTabla(")", "Símbolo (Paréntesis)");
+            imprimirTabla(")", "Símbolo (Paréntesis)", "15");
             i++;
         } else if (str[i] == '{') {
-            imprimirTabla("{", "Símbolo (Llave)");
+            imprimirTabla("{", "Símbolo (Llave)", "16");
             i++;
         } else if (str[i] == '}') {
-            imprimirTabla("}", "Símbolo (Llave)");
+            imprimirTabla("}", "Símbolo (Llave)", "17");
             i++;
         } else if (str[i] == '[') {
-            imprimirTabla("[", "Símbolo (Corchete)");
+            imprimirTabla("[", "Símbolo (Corchete)", "24");
             i++;
         } else if (str[i] == ']') {
-            imprimirTabla("]", "Símbolo (Corchete)");
+            imprimirTabla("]", "Símbolo (Corchete)", "25");
             i++;
         } else if (str[i] == ';') {
-            imprimirTabla(";", "Símbolo (Punto y Coma)");
+            imprimirTabla(";", "Símbolo (Punto y Coma)", "12");
             i++;
         }
-        // Verificar si es una palabra clave o identificador
-        else if (isalpha(str[i]) || str[i] == '_') {
+        else if (str[i] == ',') {
+            imprimirTabla(",", "Coma", "13");
+            i++;
+        }
+        // Verificar si es una palabra clave o identificador y verifica los números
+        else if (isalpha(str[i]) || str[i] == '_' || isdigit(str[i]) || isdigit(str[i]) || str[i] == '-' || str[i] == '.') {
             int j = i;
-            while (j < len && (isalnum(str[j]) || str[j] == '_')) {
+            while (j < len && (isalnum(str[j]) || str[j] == '_' || str[j] == '.')) {
                 j++;
             }
             char palabra[100];
             strncpy(palabra, &str[i], j - i);
             palabra[j - i] = '\0';
             if (esPalabraClave(palabra)) {
-                imprimirTabla(palabra, "Palabra Clave");
-            } else {
-                imprimirTabla(palabra, "Identificador");
+                imprimirTabla(palabra, "Palabra Clave", "CAMBIAR");
+            }
+            else if (esTipo(palabra)) {
+                imprimirTabla(palabra, "Tipo de dato", "4");
+            }
+            else {
+                if (!palabra[0] == '\0' && isdigit(palabra[0]) || esNumeroDecimal(palabra) || contienePuntos(palabra)) {
+                    if (contieneLetras(palabra)) {
+                        imprimirTabla(palabra, "Identificador no valido", "ERROR");
+                    }
+                }
+                else {
+                    imprimirTabla(palabra, "Identificador", "0");
+                }
+            if (esNumero(palabra)) {
+                imprimirTabla(palabra, "Número Entero", "1");
+            }
+            else if (esNumeroDecimal(palabra)) {
+                imprimirTabla(palabra, "Número Decimal", "2");
+            }
+            else if (contienePuntos(palabra)){
+                imprimirTabla(palabra, "ERROR.", "ERROR");
+            }
             }
             i = j;
         }
-        // Verificar si es un número
-        else if (isdigit(str[i]) || str[i] == '-' || str[i] == '.') {
-            int j = i;
-            while (j < len && (isdigit(str[j]) || str[j] == '.')) {
-                j++;
-            }
-            char numero[100];
-            strncpy(numero, &str[i], j - i);
-            numero[j - i] = '\0';
-            if (esNumero(numero)) {
-                imprimirTabla(numero, "Número Entero");
-            } else if (esNumeroDecimal(numero)) {
-                imprimirTabla(numero, "Número Decimal");
-            }
-            i = j;
-        } else {
-            // Carácter no reconocido, avanzar al siguiente
+        else {
             i++;
         }
     }
@@ -209,7 +252,7 @@ int main() {
     cout << "Línea a analizar: ";
     cin.getline(codigo, sizeof(codigo));
 
-    cout << setw(25) << "Elemento" << " => " << setw(20) << "Tipo de Token" << endl;
+    cout << setw(25) << "Elemento" << " => " << setw(20) << "Tipo de Token" << setw(25) << "#" << endl;
     cout << "---------------------------------------------------------" << endl;
 
     analizar(codigo);
@@ -220,4 +263,4 @@ int main() {
 
 // codigo de prueba para el analizador:
 // if (a==b) { cout "hola"; }
-// if (a == b) {     cout << "Hola, mundo!" << endl;     int x = 10;     float y = 3.14; } 
+// if (a == b) {     cout << "Hola, mundo!" << endl;     int x = 10;     float y = 3.14; }
